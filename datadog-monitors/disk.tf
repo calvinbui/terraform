a5a0d4fa0980186be8788c58f562a6dd7c17693d
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2019-12-27 04:05:30 +1100

    disk

--- datadog-monitors/disk.tf
+++ datadog-monitors/disk.tf
@@ -3,7 +3,7 @@ resource "datadog_monitor" "disk-low" {
   type    = "metric alert"
   message = var.notify_email
 
-  query = "min(last_5m):sum:system.disk.in_use{!device:shm,!device:tmpfs,!device:overlay} by {device,host} > 0.9"
+  query = "min(last_5m):sum:system.disk.in_use{*} by {device,host} > 0.9"
 
   thresholds = {
     critical = 0.9
