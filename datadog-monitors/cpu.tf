d24f80a934fc78d2091356300f28e24a829c0e3c
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2019-12-27 03:35:28 +1100

    fix

--- datadog-monitors/cpu.tf
+++ datadog-monitors/cpu.tf
@@ -3,7 +3,7 @@ resource "datadog_monitor" "cpu-high" {
   type    = "metric alert"
   message = var.notify_email
 
-  query = "sum(last_5m):sum:system.load.norm.1{*} by {host} > 0.9"
+  query = "min(last_5m):sum:system.load.norm.1{*} by {host} > 0.9"
 
   thresholds = {
     critical = 0.9
